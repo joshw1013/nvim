46bba924ab1f53d41c1fa5399a7fc36233db7442
joshw1013 <95204008+joshw1013@users.noreply.github.com>
2025-02-07 04:02:34 -0500

    Add more colorschemes

--- lua/josh/core/keymaps.lua
+++ lua/josh/core/keymaps.lua
@@ -21,6 +21,8 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader><", "10<C-w><", { desc = "Decrease window width" })
+keymap.set("n", "<leader>>", "10<C-w>>", { desc = "Increase window width" })
 
 keymap.set("n", "<leader>lf", function()
 	vim.diagnostic.enable(not vim.diagnostic.is_enabled({ bufnr = 0 }), { bufnr = 0 })
